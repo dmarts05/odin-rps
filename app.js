febfd09869f146e3850b91b6c1195abaea45c3a2
dmarts05 <dmarts05@estudiantes.unileon.es>
2022-04-16 16:07:59 +0200

    Add method to generate ai selection

--- app.js
+++ app.js
@@ -5,12 +5,30 @@ let playerScore = 0;
 let aiScore = 0;
 
 function checkValidSelection(selection) {
-  return ((selection != "rock") && (selection != "scissors") && (selection != "paper"));
+  return selection != "rock" && selection != "scissors" && selection != "paper";
 }
 
 function askPlayerSelection() {
   do {
     playerSelection = prompt("Rock, Paper or Scissors? ");
-    if (playerSelection != null) playerSelection = playerSelection.toLowerCase();
+    if (playerSelection != null)
+      playerSelection = playerSelection.toLowerCase();
   } while (checkValidSelection(playerSelection));
+}
+
+function generateAiSelection() {
+  const randNum = Math.floor(Math.random() * 3);
+  switch (randNum) {
+    case 0:
+      aiSelection = "rock";
+      break;
+    case 1:
+      aiSelection = "paper";
+      break;
+    case 2:
+      aiSelection = "scissors";
+      break;
+    default:
+      aiSelection = "error";
+  }
 }
